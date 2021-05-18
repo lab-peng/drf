1121fcef3e880b2058ec73e6344180b120dbc6a7
Robert <panerobert@yahoo.com>
2021-05-18 11:22:55 +0800

    Tutorial Five Finished

--- drf02/settings.py
+++ drf02/settings.py
@@ -127,4 +127,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# test
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
